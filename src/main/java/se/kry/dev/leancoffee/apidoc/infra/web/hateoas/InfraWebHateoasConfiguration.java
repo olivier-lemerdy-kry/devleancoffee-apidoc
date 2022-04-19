a6ba67c5873a0547e9a17b4e8c30262061315081
Olivier Le Merdy <olivier.lemerdy@kry.se>
2022-04-19 22:58:51 +0200

    Enable HAL Forms

--- src/main/java/se/kry/dev/leancoffee/apidoc/infra/web/hateoas/InfraWebHateoasConfiguration.java
+++ src/main/java/se/kry/dev/leancoffee/apidoc/infra/web/hateoas/InfraWebHateoasConfiguration.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @Configuration
-@EnableHypermediaSupport(type = {
-    EnableHypermediaSupport.HypermediaType.HAL,
-    EnableHypermediaSupport.HypermediaType.HAL_FORMS})
+@EnableHypermediaSupport(type = {EnableHypermediaSupport.HypermediaType.HAL_FORMS})
 public class InfraWebHateoasConfiguration {
 }
